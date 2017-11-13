f25a3430dedaedfc81790d7f2017b6ae4e618949
Mark.Fieldhouse@mafitsulting.co.uk <root@puppet.localdomain>
2017-11-13 15:19:47 +0000

    added new manifest

--- test_run_container.pp
+++ test_run_container.pp
@@ -1,4 +1,4 @@
-docker::eun { 'mycontainertest':
+docker::run { 'mycontainertest':
   ensure  => 'present',
   image   => 'ubuntu',
   command => '/bin/sh -c "while true; do echo test container sleep 1; done"',
